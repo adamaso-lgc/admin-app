906fb430325dbb49c88b243cc5ffa2cb90b51fed
Andre Damaso <adamaso@letsgetchecked.com>
2024-04-29 15:08:45 +0100

    default projects

--- .terraform/environments/local/main.tf
+++ .terraform/environments/local/main.tf
@@ -1,6 +1,12 @@
 locals {
   service_name = "admin-poc"
   environment  = "local" 
+
+  projects = {
+    "001" = { projectId = "001", version = "1.0", name = "Project Alpha", status = "Active" },
+    "002" = { projectId = "002", version = "1.0", name = "Project Beta", status = "Inactive" },
+    "003" = { projectId = "003", version = "1.0", name = "Project Gamma", status = "Active" }
+  }
 }
 
 module "dynamodb_table_poc" {
@@ -20,4 +26,18 @@ module "dynamodb_table_poc" {
   }
 
   stream_enabled = false
+}
+
+resource "aws_dynamodb_table_item" "default_item" {
+  for_each   = local.projects
+  table_name = module.dynamodb_table_poc.dynamodb_table_name
+  hash_key   = module.dynamodb_table_poc.dynamodb_table_hash_key
+  range_key  = module.dynamodb_table_poc.dynamodb_table_range_key
+
+  item = jsonencode({
+    projectId = {"S": each.value.projectId},
+    version   = {"S": each.value.version},
+    name      = {"S": each.value.name},
+    status    = {"S": each.value.status}
+  })
 }
--- .terraform/modules/dynamodb_generic/outputs.tf
+++ .terraform/modules/dynamodb_generic/outputs.tf
@@ -2,6 +2,10 @@ output "dynamodb_table_arn" {
   value = aws_dynamodb_table.generic_table.arn
 }
 
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.generic_table.name
+}
+
 output "dynamodb_table_tags" {
   value = aws_dynamodb_table.generic_table.tags
 }
@@ -12,4 +16,12 @@ output "dynamodb_table_tags_all" {
 
 output "dynamodb_table_stream_arn" {
   value = aws_dynamodb_table.generic_table.stream_arn
+}
+
+output "dynamodb_table_hash_key" {
+  value = aws_dynamodb_table.generic_table.hash_key
+}
+
+output "dynamodb_table_range_key" {
+  value = aws_dynamodb_table.generic_table.range_key
 }
